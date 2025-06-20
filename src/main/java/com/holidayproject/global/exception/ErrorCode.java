7693bc61a55b4104f0976e9d47e1b6282a3cb691
5jeong <wjdgns4385@naver.com>
2025-06-21 00:35:20 +0900

    ✨ [feat] 에러코드 추가(외부 API예러)

--- src/main/java/com/holidayproject/global/exception/ErrorCode.java
+++ src/main/java/com/holidayproject/global/exception/ErrorCode.java
@@ -10,7 +10,8 @@ public enum ErrorCode {
 
     // 1000번대 - 공통 에러
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 1000, "예상치 못한 서버 오류입니다."),
-    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, 1003, "잘못된 값을 입력했습니다.");
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, 1001, "잘못된 값을 입력했습니다."),
+    NAGER_DATE_API_ERROR(HttpStatus.BAD_REQUEST,1002,"Nager Date API 오류입니다.");
 
     // 2000번대 - 공휴일 관련 에러
 
