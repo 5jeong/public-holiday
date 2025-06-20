99809e00312c586fe41e69b307ae3169e3e3ed6a
5jeong <wjdgns4385@naver.com>
2025-06-21 00:33:06 +0900

    ✨ [feat] CountryRepository 작성

---
+++ src/main/java/com/holidayproject/domain/country/repository/CountryRepository.java
@@ -0,0 +1,7 @@
+package com.holidayproject.domain.country.repository;
+
+import com.holidayproject.domain.country.entity.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CountryRepository extends JpaRepository<Country, String> {
+}
