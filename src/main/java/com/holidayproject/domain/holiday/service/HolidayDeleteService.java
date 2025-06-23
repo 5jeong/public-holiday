b25fa6f8f8f8f2c482f2990db0ffe65e09d68653
5jeong <wjdgns4385@naver.com>
2025-06-24 03:27:13 +0900

    ✨ [feat] deleteByYearAndCountry 요청 year값 유효성 검사 추가

--- src/main/java/com/holidayproject/domain/holiday/service/HolidayDeleteService.java
+++ src/main/java/com/holidayproject/domain/holiday/service/HolidayDeleteService.java
@@ -1,6 +1,7 @@
 package com.holidayproject.domain.holiday.service;
 
 import com.holidayproject.domain.country.repository.CountryRepository;
+import com.holidayproject.domain.holiday.dto.response.SuccessMessageResponse;
 import com.holidayproject.domain.holiday.repository.HolidayRepository;
 import com.holidayproject.global.exception.BusinessException;
 import com.holidayproject.global.exception.ErrorCode;
@@ -14,15 +15,28 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 public class HolidayDeleteService {
 
+    private static final int MIN_YEAR = 1975;
+    private static final int MAX_YEAR = 2075;
+    public static final String DELETE_SUCCESS_MESSAGE = "%d, %s 데이터 레코드 삭제 완료";
     private final HolidayRepository holidayRepository;
     private final CountryRepository countryRepository;
 
     @Transactional
-    public void deleteByYearAndCountry(int year, String countryCode) {
+    public SuccessMessageResponse deleteByYearAndCountry(int year, String countryCode) {
+
+        validateRequest(year, countryCode);
+        holidayRepository.deleteByYearAndCountryCode(year, countryCode);
+
+        log.info("연도: {}, 국가: {} 공휴일 전체 삭제 완료", year, countryCode);
+        return SuccessMessageResponse.of(String.format(DELETE_SUCCESS_MESSAGE, year, countryCode));
+    }
+
+    private void validateRequest(int year, String countryCode) {
+        if (year < MIN_YEAR || year > MAX_YEAR) {
+            throw new BusinessException(ErrorCode.INVALID_YEAR);
+        }
         if (!countryRepository.existsByCountryCode(countryCode)) {
             throw new BusinessException(ErrorCode.INVALID_COUNTRY_CODE);
         }
-        holidayRepository.deleteByYearAndCountryCode(year, countryCode);
-        log.info("연도: {}, 국가: {} 공휴일 전체 삭제 완료", year, countryCode);
     }
 }
