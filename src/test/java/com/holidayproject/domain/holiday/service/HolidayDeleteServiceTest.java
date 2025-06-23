8bc309f44b891cd3cb80f954d8034aa53c2fddf5
5jeong <wjdgns4385@naver.com>
2025-06-24 03:27:40 +0900

    ✅ [test]  deleteByYearAndCountry 요청 year값 유효성 검사 추가로 인한 테스트 코드 작성

--- src/test/java/com/holidayproject/domain/holiday/service/HolidayDeleteServiceTest.java
+++ src/test/java/com/holidayproject/domain/holiday/service/HolidayDeleteServiceTest.java
@@ -4,14 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.holidayproject.IntegrationTestSupport;
-import com.holidayproject.domain.country.repository.CountryRepository;
 import com.holidayproject.domain.holiday.entity.Holiday;
 import com.holidayproject.domain.holiday.repository.HolidayRepository;
 import com.holidayproject.global.exception.BusinessException;
 import com.holidayproject.global.exception.ErrorCode;
 import java.time.LocalDate;
 import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,7 +26,7 @@ class HolidayDeleteServiceTest extends IntegrationTestSupport {
     private HolidayRepository holidayRepository;
 
     @Test
-    @DisplayName("존재하는 국가 코드일 경우 공휴일 삭제에 성공한다")
+    @DisplayName("존재하는 국가 코드,유효한 년도일 경우 공휴일 삭제에 성공한다")
     void delete_success() {
         //given
         holidayRepository.save(Holiday.builder()
@@ -67,6 +65,22 @@ class HolidayDeleteServiceTest extends IntegrationTestSupport {
         assertThatThrownBy(() -> holidayDeleteService.deleteByYearAndCountry(2024, "NOT_FOUND"))
                 .isInstanceOf(BusinessException.class)
                 .hasMessageContaining(ErrorCode.INVALID_COUNTRY_CODE.getMessage());
+    }
+
+    @Test
+    @DisplayName("연도가 1975 미만일 경우 예외가 발생한다")
+    void delete_fail2() {
+        assertThatThrownBy(() -> holidayDeleteService.deleteByYearAndCountry(1974, "KR"))
+                .isInstanceOf(BusinessException.class)
+                .hasMessageContaining(ErrorCode.INVALID_YEAR.getMessage());
+    }
+
+    @Test
+    @DisplayName("연도가 2075 초과일 경우 예외가 발생한다")
+    void delete_fail3() {
+        assertThatThrownBy(() -> holidayDeleteService.deleteByYearAndCountry(2076, "KR"))
+                .isInstanceOf(BusinessException.class)
+                .hasMessageContaining(ErrorCode.INVALID_YEAR.getMessage());
     }
 
 }
