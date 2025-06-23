93455a35b361f2abf233eb1ca041fddbe0ab8eea
5jeong <wjdgns4385@naver.com>
2025-06-23 18:36:19 +0900

    ✨ [feat] 공휴일 검색 관련 에러코드 추가

--- src/main/java/com/holidayproject/global/exception/ErrorCode.java
+++ src/main/java/com/holidayproject/global/exception/ErrorCode.java
@@ -11,9 +11,11 @@ public enum ErrorCode {
     // 1000번대 - 공통 에러
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 1000, "예상치 못한 서버 오류입니다."),
     INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, 1001, "잘못된 값을 입력했습니다."),
-    NAGER_DATE_API_ERROR(HttpStatus.BAD_REQUEST,1002,"Nager Date API 오류입니다.");
+    NAGER_DATE_API_ERROR(HttpStatus.BAD_REQUEST, 1002, "Nager Date API 오류입니다."),
 
-    // 2000번대 - 공휴일 관련 에러
+    // 2000번대 - 공휴일 관련 로직 에러
+    INVALID_SEARCH_CONDITION(HttpStatus.BAD_REQUEST, 2001, "year와 from/to는 동시에 검색할 수 없습니다."),
+    INVALID_DATE_RANGE(HttpStatus.BAD_REQUEST, 2002, "from 날짜는 to보다 이전이어야 합니다.");
 
     private final HttpStatus status; // HTTP 상태 코드
     private final int code; // 오류 코드
