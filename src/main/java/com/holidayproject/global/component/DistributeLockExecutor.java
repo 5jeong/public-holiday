fec3a2c9e1caf2bbe7a3907d95699db5b2d76f67
5jeong <wjdgns4385@naver.com>
2025-06-24 00:23:05 +0900

    ✨ [feat] DistributeLockExecutor (분산락 컴포넌트) 구현

---
+++ src/main/java/com/holidayproject/global/component/DistributeLockExecutor.java
@@ -0,0 +1,36 @@
+package com.holidayproject.global.component;
+
+import com.holidayproject.global.exception.BusinessException;
+import com.holidayproject.global.exception.ErrorCode;
+import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class DistributeLockExecutor {
+
+    private final RedissonClient redissonClient;
+
+    public void execute(String lockName, long waitMilliSecond, long leaseMilliSecond, Runnable logic) {
+        RLock lock = redissonClient.getLock(lockName);
+        try {
+            boolean isLocked = lock.tryLock(waitMilliSecond, leaseMilliSecond, TimeUnit.MILLISECONDS);
+            if (!isLocked) {
+                throw new BusinessException(ErrorCode.LOCK_ACQUIRE_FAIL);
+            }
+            logic.run();
+        } catch (InterruptedException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        } finally {
+            if (lock.isHeldByCurrentThread()) {
+                lock.unlock();
+            }
+        }
+    }
+}
