b218bf875e05c2758e577153475be386f563b9d4
5jeong <wjdgns4385@naver.com>
2025-06-24 23:56:36 +0900

    ✨ [feat] 외부 API spring retry(재시도) 로직 적용

--- src/main/java/com/holidayproject/domain/api/CountryApiClient.java
+++ src/main/java/com/holidayproject/domain/api/CountryApiClient.java
@@ -5,26 +5,42 @@ import com.holidayproject.global.exception.BusinessException;
 import com.holidayproject.global.exception.ErrorCode;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class CountryApiClient {
 
     private static final String COUNTRY_URL = "https://date.nager.at/api/v3/AvailableCountries";
     private final RestClient restClient;
 
+    @Retryable(
+            retryFor = {Exception.class},
+            maxAttempts = 3,
+            backoff = @Backoff(delay = 2000, multiplier = 2)
+    )
     public List<CountryDto> getAvailableCountries() {
         return restClient.get()
                 .uri(COUNTRY_URL)
                 .retrieve()
-                .onStatus(HttpStatusCode::isError,(request, response)->{
+                .onStatus(HttpStatusCode::isError, (request, response) -> {
                     throw new BusinessException(ErrorCode.NAGER_DATE_API_ERROR);
                 })
                 .body(new ParameterizedTypeReference<>() {
                 });
+    }
+
+    @Recover
+    public List<CountryDto> recover(Exception e) {
+        log.error("[nager API 실패] 국가 목록 조회 실패 : {}", e.getMessage());
+        throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
     }
 }
--- src/main/java/com/holidayproject/domain/api/HolidayApiClient.java
+++ src/main/java/com/holidayproject/domain/api/HolidayApiClient.java
@@ -5,18 +5,28 @@ import com.holidayproject.global.exception.BusinessException;
 import com.holidayproject.global.exception.ErrorCode;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class HolidayApiClient {
 
     private static final String PUBLIC_HOLIDAY_URL = "https://date.nager.at/api/v3/PublicHolidays/%d/%s";
     private final RestClient restClient;
 
+    @Retryable(
+            retryFor = {Exception.class},
+            maxAttempts = 3,
+            backoff = @Backoff(delay = 2000, multiplier = 2)
+    )
     public List<HolidayDto> getHolidays(int year, String countryCode) {
         return restClient.get()
                 .uri(String.format(PUBLIC_HOLIDAY_URL, year, countryCode))
@@ -26,5 +36,11 @@ public class HolidayApiClient {
                 })
                 .body(new ParameterizedTypeReference<>() {
                 });
+    }
+
+    @Recover
+    public List<HolidayDto> recover(Exception e, int year, String countryCode) {
+        log.error("[nager API 실패] 공휴일 조회 실패 - year: {}, countryCode: {}, 이유 : {}", year, countryCode, e.getMessage());
+        throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
     }
 }
