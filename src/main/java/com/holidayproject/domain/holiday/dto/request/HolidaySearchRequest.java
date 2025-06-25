8b03c095f1a6a03013542ce83fd10ea982827def
5jeong <wjdgns4385@naver.com>
2025-06-25 17:50:40 +0900

    ♻️ [refactor] 스웨거 문서 요청 예시 변경 리팩토링

--- src/main/java/com/holidayproject/domain/holiday/dto/request/HolidaySearchRequest.java
+++ src/main/java/com/holidayproject/domain/holiday/dto/request/HolidaySearchRequest.java
@@ -9,7 +9,7 @@ import lombok.Builder;
 @Builder
 public record HolidaySearchRequest(
 
-        @Schema(description = "조회할 연도", example = "2024")
+        @Schema(description = "조회할 연도", example = "null")
         Integer year,
 
         @Schema(description = "조회 시작일", example = "2024-01-01")
@@ -21,25 +21,25 @@ public record HolidaySearchRequest(
         @Schema(description = "공휴일 이름", example = "New Year's Day")
         String name,
 
-        @Schema(description = "로컬 공휴일 이름", example = "신정")
+        @Schema(description = "로컬 공휴일 이름", example = "새해")
         String localName,
 
         @Schema(description = "국가 코드", example = "KR")
         String countryCode,
 
-        @Schema(description = "고정 날짜 여부 (true: 매년 같은 날짜)", example = "true")
+        @Schema(description = "고정 날짜 여부 (true: 매년 같은 날짜)", example = "false")
         Boolean fixed,
 
-        @Schema(description = "글로벌 공휴일 여부", example = "false")
+        @Schema(description = "글로벌 공휴일 여부", example = "true")
         Boolean global,
 
-        @Schema(description = "공휴일이 최초 도입된 연도", example = "1950")
+        @Schema(description = "공휴일이 최초 도입된 연도", example = "null")
         Integer launchYear,
 
         @Schema(description = "공휴일이 적용되는 지역 코드 리스트", example = "[\"US-NY\", \"US-CA\"]")
         List<String> counties,
 
-        @Schema(description = "공휴일 유형 리스트 (예: PUBLIC)", example =  "[\"PUBLIC\"]")
+        @Schema(description = "공휴일 유형 리스트 (예: PUBLIC)", example =  "[\"Public\"]")
         List<String> types
 ) {
 }
