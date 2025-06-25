032b02db13b56e4991b3312d68c76dc148e3a7fe
5jeong <wjdgns4385@naver.com>
2025-06-25 16:48:51 +0900

    🐛 [fix] "text/json" 형식 convert 오류 해결

--- src/main/java/com/holidayproject/global/config/RestClientConfig.java
+++ src/main/java/com/holidayproject/global/config/RestClientConfig.java
@@ -1,8 +1,12 @@
 package com.holidayproject.global.config;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -10,6 +14,14 @@ public class RestClientConfig {
 
     @Bean
     public RestClient restClient() {
-        return RestClient.builder().build();
-    }
+        MappingJackson2HttpMessageConverter jacksonConverter = new MappingJackson2HttpMessageConverter();
+
+        // text/json 추가
+        List<MediaType> mediaTypes = new ArrayList<>(jacksonConverter.getSupportedMediaTypes());
+        mediaTypes.add(MediaType.valueOf("text/json"));
+        jacksonConverter.setSupportedMediaTypes(mediaTypes);
+
+        return RestClient.builder()
+                .messageConverters(List.of(jacksonConverter))
+                .build();    }
 }
